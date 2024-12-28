b26590183c4e1253365a242b6ca08b14d42bb6dc
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-12-28 22:34:14 +0800

    A small calculator.

---
+++ demos/calc/script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Invalid Expression');
+        clearDisplay();
+    }
+}
